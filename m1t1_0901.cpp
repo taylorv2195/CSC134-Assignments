9d101253e9953d33c1b5f9b8b60da2dca2c81a16
Vincent Taylor <taylorv2195@student.faytechcc.edu>
2025-09-08 03:31:43 +0000

    Finish m1t1

--- m1t1_0901.cpp
+++ m1t1_0901.cpp
@@ -5,3 +5,18 @@ taylorv
 09/07/2525
 Program will greet the user.
 */
+
+// magic words
+#include <iostream>
+using namespace std;
+
+int main() {
+    // input: start the program
+    // processing: none
+    // output: 0 (no errors)
+    
+
+
+
+    return 0; // no errors
+}
