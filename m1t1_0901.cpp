0d037941fe1ce00eaef85778350367ec78effe12
Vincent Taylor <taylorv2195@student.faytechcc.edu>
2025-09-08 03:36:35 +0000

    Finished m1t1!

--- m1t1_0901.cpp
+++ m1t1_0901.cpp
@@ -14,9 +14,10 @@ int main() {
     // input: start the program
     // processing: none
     // output: 0 (no errors)
-    
+    // special FX; Says "Hello" to the user.
 
-
+    // Let's say hello
+    cout <<"Hello, user!" << endl; // "end line" not "end 1"
 
     return 0; // no errors
 }
